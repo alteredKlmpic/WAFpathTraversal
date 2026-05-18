0541173381d1a1aa5c7a6e5f7ad533eb53555f2c
megaMindKlakpic <klampicyt@gmail.com>
2026-05-18 20:11:01 +0200

    konekcija sa aplikacijom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import urlib.parse
 from flask import Flask, request, Response
 
 app = Flask(__name__)
@@ -10,76 +9,20 @@ app.config['SERVER_NAME'] = '127.0.0.1:9000'
 # Gde se nalazi naša ranjiva aplikacija (Docker sada uspešno sluša na portu 80):
 TARGET_URL = "http://127.0.0.1:80"
 
-commonPathTraversalWords = frozenset(["etc","passwd","proc","root","ssh","shadow","config","//","..","main"])
-#TODO dodati white list za situacije poput http://testsite.com/get.php?f=list
-
-def decoding(putanja):
-
-    prevStr = ""
-    currStr = putanja
-
-    while prevStr != currStr:
-        prevStr = currStr
-        currStr = urllib.parse.unquote(currStr)
-
-    return currStr
-
-def toLowerCase(putanja):
-
-    lowerCaseUrl = putanja.lower()
-    return lowerCaseUrl
-
-def checkUrl(putanja):
-
-    for char in putanja:
-        if char.isupper():
-            putanja = toLowerCase(putanja)
-            break
-
-    if "%" in putanja:
-        putanja = decoding(putanja)
-
-    if any(word in putanja for word in commonPathTraversalWords):
-        return True
-
-    if putanja.count("http") > 1 or putanja.count("https") > 1 or putanja.count("php") > 1:
-        return True
-
-    return False
-
-
-
-def checkCookie():
-
-    cookie = request.cookies.get("session_token")
-
-    if "%" in cookie:
-        cookie = decoding(cookie)
-
-    if any(word in cookie for word in commonPathTraversalWords):
-        return True
-
-    return False
-
-@app.route("/admin-panel")
-def isAdmin():
-
-    if session.get("is_admin") == True:
-        return True
-    else:
-        return False
 
 def proveri_saobracaj(putanja, parametri):
     """
     WAF logika za inspekciju saobraćaja.
     Ako detektuje '..' u putanji ili u bilo kom GET parametru, vraća True (Blokiraj).
     """
+    # 1. Provera same putanje u URL-u
+    if ".." in putanja:
+        return True
 
-    if not isAdmin():
-       if checkUrl(putanja) or checkCookie():
-           return True
-
-
+    # 2. Provera svih GET parametara (npr. ?page=../../)
+    for kljuc, vrednost in parametri.items():
+        if ".." in str(vrednost):
+            return True
 
     return False
 
