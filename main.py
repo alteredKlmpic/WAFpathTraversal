da24a5493679b34c7f8bf63b63d37e9bacf3cb40
megaMindKlakpic <klampicyt@gmail.com>
2026-05-18 21:34:46 +0200

    napravljena always on konekcija i timeout error response

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from flask import Flask, request, Response
 
 app = Flask(__name__)
 
-# Eksplicitno podešavamo SERVER_NAME u Flasku da ga nateramo na port 9000
-app.config['SERVER_NAME'] = '127.0.0.1:9000'
+veza_sa_dockerom = requests.Session()
 
-# Gde se nalazi naša ranjiva aplikacija (Docker sada uspešno sluša na portu 80):
-TARGET_URL = "http://127.0.0.1:80"
+app.config['SERVER_NAME'] = '127.0.0.1:9000' #waf
+TARGET_URL = "http://127.0.0.1:80" #dvwa
 
 
 def proveri_saobracaj(putanja, parametri):
@@ -37,39 +36,43 @@ def proxy(path):
     if proveri_saobracaj(path, parametri):
         # Ako je maliciozno, vraćamo 403 Forbidden i ispisujemo u konzoli
         print(f"\n[WAF DETEKCIJA] BLOKIRAN NAPAD! Putanja: /{path} | Parametri: {dict(parametri)}")
-        return "<h1>403 Forbidden</h1><p>WAF je uspesno blokirao tvoj Path Traversal napad!</p>", 403
+        return "403 Forbidden, WAF je blokirao napad", 403
 
     # Ako je sve u redu, konstruišemo URL za Docker
     url = f"{TARGET_URL}/{path}"
 
     # Pravilno čišćenje zaglavlja
-    zaglavlja = {k: v for k, v in request.headers.items() if k.lower() != 'host'}
-    zaglavlja = {k: v for k, v in zaglavlja.items() if k.lower() != 'accept-encoding'}
+    zaglavlja = {key: value for key, value in request.headers.items() if key.lower() != 'host'}
+    zaglavlja = {key: value for key, value in zaglavlja.items() if key.lower() != 'accept-encoding'}
 
     # Slanje zahteva Docker aplikaciji na portu 80
     try:
-        odgovor_od_dvwa = requests.request(
+        odgovor_od_dvwa = veza_sa_dockerom.request(
             method=request.method,
             url=url,
             headers=zaglavlja,
             params=parametri,
             data=request.get_data(),
             cookies=request.cookies,
-            allow_redirects=False
+            allow_redirects=False,
+            timeout = 5
         )
+    except requests.exceptions.Timeout:
+        return "504 Gateway Timeout, previse vremena je trebalo", 504
     except requests.exceptions.ConnectionError:
-        return "<h1>502 Bad Gateway</h1><p>WAF ne moze da komunicira sa Docker aplikacijom. Proveri da li je Docker pokrenut.</p>", 502
+        return "502 Bad Gateway, problem u komunikaciji sa dockerom ", 502
+
 
     # Vraćamo odgovor iz Dockera nazad korisniku u browser
     response = Response(odgovor_od_dvwa.content, odgovor_od_dvwa.status_code)
-    for k, v in odgovor_od_dvwa.headers.items():
-        if k.lower() not in ['content-length', 'connection', 'transfer-encoding', 'content-encoding']:
-            response.headers[k] = v
+    for key, value in odgovor_od_dvwa.headers.items():
+        if key.lower() not in ['content-length', 'connection', 'transfer-encoding', 'content-encoding']:
+            response.headers[key] = value
 
     return response
 
 
 if __name__ == "__main__":
-    print("[WAF] Pokrenut na http://127.0.0.1:9000 -> Štiti DVWA na portu 80")
+    print("[WAF] Pokrenut na http://127.0.0.1:9000 -> DVWA na portu 80")
     # Pokrećemo bez debug moda da PyCharm ne bi forsirao stare portove
     app.run(threaded=True, debug=False)
